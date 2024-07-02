94bad6733bc083d92cc835435bc37bc01d9d19f4
kamalgmbh <kamalgmbh32@gmail.com>
2024-07-03 00:14:36 +0530

    Update setup_siena.sh

--- my-meta-amd/setup_siena.sh
+++ my-meta-amd/setup_siena.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-YOCTO_BRANCH="kirkstone"
+YOCTO_BRANCH="scarthgap"
 REPO_DIR="poky-amd-${YOCTO_BRANCH}"
 
 # Clone poky repository
@@ -14,23 +14,42 @@ git clone --single-branch --branch "${YOCTO_BRANCH}" "git://git.openembedded.org
 git clone --single-branch --branch "${YOCTO_BRANCH}" "git://git.yoctoproject.org/meta-dpdk"
 
 # Clone meta-virtualization for enable virtualization or libvirt
-git clone https://git.yoctoproject.org/meta-virtualization -b kirkstone
+git clone https://git.yoctoproject.org/meta-virtualization -b scarthgap
 
 # Clone meta-secure-core for tpm tools
 #git clone -b master https://github.com/Wind-River/meta-secure-core.git
 
 # Clone meta-amd repository using SSH
-git clone "ssh://gerritgit/eesc/ec/ese/meta-amd"
+git clone "ssh://gerritgit/eesc/ec/ese/meta-amd" -b kirkstone
 # Clone for tpm-tools
 #git clone -b kirkstone https://github.com/Wind-River/meta-secure-core.git
-git clone -b kirkstone https://git.yoctoproject.org/git/meta-security
+git clone -b scarthgap  https://git.yoctoproject.org/git/meta-security
 
-# Checkout specific tags and branches
-git checkout --quiet tags/yocto-4.0.9
+cd meta
+git checkout --quiet   f7def85be9f99dcb4ba488bead201f670304379b
 cd meta-openembedded
-git checkout --quiet 402affcc073db39f782c1ebfd718edd5f11eed4c
-cd ../meta-dpdk
-git checkout --quiet 12cd9701455f17ff86681dba86e5c10f7b0deb7c
-cd ../meta-amd
-git checkout kirkstone
+git checkout --quiet   4a7bb77f7ebe0ac8be5bab5103d8bd993e17e18d
+
+cd meta-poky
+git checkout --quiet  f7def85be9f99dcb4ba488bead201f670304379b
+
+cd meta-security
+git checkout --quiet 11ea91192d43d7c2b0b95a93aa63ca7e73e38034
+
+cd meta-selftest
+git checkout --quiet  f7def85be9f99dcb4ba488bead201f670304379b
+
+cd meta-skeleton
+git checkout --quiet  f7def85be9f99dcb4ba488bead201f670304379b
+
+cd meta-virtualization
+git checkout --quiet 37c06acf58f9020bccfc61954eeefe160642d5f3
+
+cd meta-vulkan
+git checkout --quiet   3d722da4e5fb2b0f82b537c21e41022c3772c2dc
+
+cd meta-yocto-bsp
+git checkout --quiet f7def85be9f99dcb4ba488bead201f670304379b
+
+
 
